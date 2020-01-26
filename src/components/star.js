bd6b29dd3be01938101f54a90323f086bfa15dcf
abhishek_Bhosale <abhishek11521@gmail.com>
2020-01-26 17:54:08 +0530

    multiple state, star rating has own state for ratings

--- src/components/star.js
+++ src/components/star.js
@@ -3,6 +3,6 @@ import { FaStar } from "react-icons/fa";
 
 export default function Star({ selected = false, onselect = f => f }) {
   return (
-    <FaStar color={selected ? "red" : "grey"} size="100px" onClick={onselect} />
+    <FaStar color={selected ? "red" : "grey"} size="50px" onClick={onselect} />
   );
 }
